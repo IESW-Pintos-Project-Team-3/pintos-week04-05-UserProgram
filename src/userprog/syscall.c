3f6063a048e47ce4c993bea47fe035716396ce6d
hkmaeng <hkmaeng9075@naver.com>
2025-07-22 16:51:43 +0900

    "feat:open, close, tell 구현"

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,8 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -15,6 +17,39 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  switch (*((uint32_t*)f->esp)){
+    case SYS_CREATE:
+    case SYS_OPEN:
+      char* file_name = *(char**)(f->esp + 4);
+      struct file* file = filesys_open(file_name);
+      if (file == NULL){
+        f->eax = -1;
+      }
+      else{
+        f->eax = allocate_fd(file);
+      }
+      break;
+    case SYS_READ:
+    case SYS_WRITE:
+    case SYS_SEEK:
+    case SYS_CLOSE:
+      int fd = *(int *)(f->esp+4);
+      file_close(get_file(fd));
+      break;
+    case SYS_FILESIZE:
+      
+    case SYS_TELL:
+      int fd = *(int *)(f->esp+4);
+      struct file* file = get_file(fd);
+      if (file == NULL){
+        f->eax = -1;
+      }
+      else{
+        f->eax = file->pos;
+      }
+      break;
+    case SYS_REMOVE:
+  }
+  // printf ("system call!\n");
+  // thread_exit ();
 }
