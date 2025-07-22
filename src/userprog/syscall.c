9d82ea7280c182798b815c98a1040654e08bdb41
thisjheok <jhjang3344@gmail.com>
2025-07-22 15:16:40 +0900

    feat:syscall_write

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "lib/user/syscall.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -15,6 +16,22 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+  int syscall_number = *(int*)f->esp;
+  switch (syscall_number)
+  {
+    case SYS_WRITE:
+      int fd = *(int*)(f->esp + 4);        // 첫 번째 인자
+      void *buffer = *(void**)(f->esp + 8); // 두 번째 인자  
+      unsigned size = *(unsigned*)(f->esp + 12); // 세 번째 인자
+
+      if(fd == STDOUT_FILENO){
+        putbuf(buffer,size);
+        f->eax = size;
+      }else{
+        f->eax = -1;
+      }
+      break;
+  }
   printf ("system call!\n");
   thread_exit ();
 }
